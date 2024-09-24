b514b7da1d3be3de8dc732cce3d7fdca005d88a1
Phu Binh Dang <Phu.Binh.Dang@wps.de>
2024-09-24 15:33:00 +0200

    comment out MetricLogger (probably caused error inDataWorker)

--- engine.py
+++ engine.py
@@ -21,25 +21,32 @@ import util.misc as utils
 from datasets.tad_eval import TADEvaluator
 import pickle
 
+
 def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
                     data_loader: Iterable, optimizer: torch.optim.Optimizer,
                     device: torch.device, epoch: int, cfg, max_norm: float = 0):
     model.train()
     criterion.train()
 
-    metric_logger = utils.MetricLogger(delimiter="  ")
-    metric_logger.add_meter('lr', utils.SmoothedValue(
-        window_size=1, fmt='{value:.6f}'))
-    header = 'Epoch: [{}]'.format(epoch)
-    print_freq = 20
+    # TOOD: fix exited debugger with no reason
+    # metric_logger = utils.MetricLogger(delimiter="  ")
+    # metric_logger.add_meter('lr', utils.SmoothedValue(
+    #     window_size=1, fmt='{value:.6f}'))
+    # header = 'Epoch: [{}]'.format(epoch)
+    # print_freq = 20
     cnt = 0
 
-    for samples, targets in metric_logger.log_every(data_loader, print_freq, header):
+    # for samples, targets in metric_logger.log_every(data_loader, print_freq, header):
+    for samples, targets in tqdm.tqdm(data_loader, total=len(data_loader)):
         samples = samples.to(device)
         targets = [{k: v.to(device) if k in ['segments', 'labels']
-                    else v for k, v in t.items()} for t in targets]
-
+        else v for k, v in t.items()} for t in targets]
+        # sample: (
+        # tensors (4,3,256,96,96)
+        # mask (4,256)
+        # )
         outputs = model((samples.tensors, samples.mask))
+
         loss_dict = criterion(outputs, targets)
         weight_dict = criterion.weight_dict
         losses = sum(loss_dict[k] * weight_dict[k]
@@ -74,17 +81,19 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
             optimizer.step()
             optimizer.zero_grad()
 
-        metric_logger.update(
-            loss=loss_value, **loss_dict_reduced_scaled, **loss_dict_reduced_unscaled)
-        metric_logger.update(lr=optimizer.param_groups[0]["lr"])
+        # metric_logger.update(
+        #     loss=loss_value, **loss_dict_reduced_scaled, **loss_dict_reduced_unscaled)
+        # metric_logger.update(lr=optimizer.param_groups[0]["lr"])
 
         cnt += 1
 
     optimizer.zero_grad()
     # gather the stats from all processes
     metric_logger.synchronize_between_processes()
-    logging.info(f"Averaged stats:{metric_logger}")
-    return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
+    # logging.info(f"Averaged stats:{metric_logger}")
+    # return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
+    # FIXME
+    return {}
 
 
 def to_device(t, device):
@@ -95,7 +104,8 @@ def to_device(t, device):
 
 
 @torch.no_grad()
-def test(model, criterion, postprocessor, data_loader, base_ds, device, output_dir, cfg, subset='val', epoch=None, test_mode=False):
+def test(model, criterion, postprocessor, data_loader, base_ds, device, output_dir, cfg, subset='val', epoch=None,
+         test_mode=False):
     '''
     Run inference and evaluation. Do not compute loss
     test_mode: indicates that we are evaluating specific epoch during testing
@@ -108,12 +118,12 @@ def test(model, criterion, postprocessor, data_loader, base_ds, device, output_d
         window_size=1, fmt='{value:.2f}'))
 
     iou_range = [0.3, 0.4, 0.5, 0.6, 0.7] if cfg.dataset_name == 'thumos14' else [
-        num/100 for num in range(50, 100, 5)]
+        num / 100 for num in range(50, 100, 5)]
     # logging.info('iou range {}'.format(iou_range))
 
     # action_evaluator = None
     action_evaluator = TADEvaluator(cfg.dataset_name, subset, base_ds, nms_mode=[
-                                          'raw'], iou_range=iou_range, epoch=epoch)
+        'raw'], iou_range=iou_range, epoch=epoch)
 
     # raw_res = []
     cnt = 0
@@ -127,7 +137,7 @@ def test(model, criterion, postprocessor, data_loader, base_ds, device, output_d
         results = postprocessor(outputs, video_duration, fuse_score=cfg.act_reg)
 
         res = {target['video_id']: output for target,
-               output in zip(targets, results)}
+        output in zip(targets, results)}
         if action_evaluator is not None:
             action_evaluator.update(res, assign_cls_labels=cfg.binary)
         # if cnt >= 9:
@@ -151,8 +161,8 @@ def test(model, criterion, postprocessor, data_loader, base_ds, device, output_d
             for vk, vv in v.items():
                 stats[vk + '_' + k] = vv
 
-        mAP_values = ' '.join([f'{k}: {100*v:.2f}'.format(k, v)
-                              for k, v in stats.items() if k.startswith('mAP')])
+        mAP_values = ' '.join([f'{k}: {100 * v:.2f}'.format(k, v)
+                               for k, v in stats.items() if k.startswith('mAP')])
         logging.info(mAP_values)
 
         stats['stats_summary'] = action_evaluator.stats_summary
